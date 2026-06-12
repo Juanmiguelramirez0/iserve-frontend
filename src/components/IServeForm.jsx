56ddd5b43a226e761aa9462b44236b382b3e9e0b
Juan Miguel Ramirez <ramirezjuanmiguel032@gmail.com>
2026-06-13 00:36:17 +0800

    Fixing filename case for Vercel

--- src/components/IServeForm.jsx
+++ src/components/IServeForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { submitRequest } from '../api';
+import IServeForm from "./components/IServeForm";
 import { 
   ClipboardList, 
   Send, 
